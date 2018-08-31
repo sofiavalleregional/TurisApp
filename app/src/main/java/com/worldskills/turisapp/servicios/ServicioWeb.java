6d895bc6550b830d61f9617324e431709c8744a8
alexander casanova <casanova8526@gmail.com>
2018-08-31 08:41:19 -0500

    alex

--- app/src/main/java/com/worldskills/turisapp/servicios/ServicioWeb.java
+++ app/src/main/java/com/worldskills/turisapp/servicios/ServicioWeb.java
@@ -1,5 +1,9 @@
 package com.worldskills.turisapp.servicios;
 
+//https://maps.googleapis.com/maps/api/directions/json?origin=parametroLatitud,parametroLongitud&destination=parametroLatitud,parametroLongitud
+
+
+//Esta interfaz se encarga de hacer los diferentes pedidos a los servicios web, ya se para los lugares o las rutas
 public interface ServicioWeb {
 
 
