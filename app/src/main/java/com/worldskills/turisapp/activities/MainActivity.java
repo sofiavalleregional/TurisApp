1385bce02410899f431b5f0df8adff8a87302ff0
alexander casanova <casanova8526@gmail.com>
2018-08-31 09:08:23 -0500

    alex

--- app/src/main/java/com/worldskills/turisapp/activities/MainActivity.java
+++ app/src/main/java/com/worldskills/turisapp/activities/MainActivity.java
@@ -82,17 +82,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_si) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_inicio) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_sitios) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_hoteles) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_restaurantes) {
 
         }
 
